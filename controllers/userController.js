63ba488a4e86b135f4c474e56ca74eccb5c2cdb0
oladokunlt <lukmanoladokun86@gmail.com>
2024-07-10 19:05:09 +0100

    Added route and controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,33 @@ const getAllUsers = async (req, res) => {
   res.send(await userCollection.find({}));
 };
 
+const getSingleUser = async (req, res) => {
+  const user = await userCollection.findById(req.params.id);
+
+  res.send(user);
+};
+
+const updateUser = async (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  await userCollection.findByIdAndUpdate(req.params.id, {
+    email,
+    password: hashedPassword,
+  });
+
+  res.send("user updated successfully");
+};
+
+const deleteUser = async (req, res) => {
+  await userCollection.findByIdAndDelete(req.params.id);
+
+  res.send("user deleted");
+};
+
 module.exports = {
   register,
   getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { register, getAllUsers } = require("../controllers/userController");
+const {
+  register,
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
 const userRoute = express.Router();
 
 userRoute.post("/register", register);
 userRoute.get("/", getAllUsers);
+userRoute.get("/user/:id", getSingleUser);
+userRoute.put("/user/:id", updateUser);
+userRoute.delete("/user/:id", deleteUser);
 
 module.exports = userRoute;
